8bae2d0648d4fa28a342b5e77f816bdde31b1867
BEN SAID Walid <wbensaid@pcj404.psi.ad.unistra.fr>
2021-10-14 12:25:24 +0200

    Finir Exercice 2

--- easywebgl/TP4_ex2.js
+++ easywebgl/TP4_ex2.js
@@ -8,38 +8,38 @@ var vertexShader =
 `#version 300 es
 
 // INPUT
-// - the currently bounded vertex array (VAO) contains a VBO of 3D data (positions)
-// - variable is prefixed by "in"
-// - its "location index" MUST be the same value when using vertexAttribPointer() and enableVertexAttribArray() during VAO definition
-layout(location=1) in vec3 position_in;
-layout(location=3) in vec2 textureCoord_in;
+// - nothing ! this is purely procedural geometry
 
 // UNIFORM
 // - variable is prefixed by "uniform"
 // - "uniforms" are variables on GPU in "constant memory" => there values are constant during a "draw command" such as drawArrays()
 // - they can be seen as user custom parameters of your shaders
 // - they can be accessed in any shader (vertex, fragment)
-// - Camera
-uniform mat4 uProjectionMatrix;
-uniform mat4 uViewMatrix;
-// - Model
-uniform mat4 uModelMatrix;
-
-// OUTPUT
-out vec2 v_textureCoord;
+uniform float uTime;
+uniform int uNbVertices;
+uniform int uNbInstances;
 
 // MAIN PROGRAM
 void main()
 {
-	// Send data to graphics pipeline
-	v_textureCoord = textureCoord_in;
-
+	// "gl_PointSize" is a predined variable in GLSL to fix the size of primitives of type "GL_POINTS"
+	// it wiil be used by the "rasterizer" to generate points of given size in "pixels"
+	// mix(min, max, parameter) is used to "blend" linearly between "min" and "max" accordning to a parameter in [0.0;1.0]
+	gl_PointSize = mix(5.0/*min*/, 50.0/*max*/, 0.5 * sin(uTime) + 0.5);
+	
 	// MANDATORY
 	// - a vertex shader MUST write the value of the predined variable " (GLSL langage)"
-	// - this value represent a position in "clip-space"
-	// - This is the space just before dividing coordinates "xyz" by their "w" to lie in NDC space (normalized device coordinates),
-	// - i.e a cube in [-1.0;1.0]x[-1;1.0]x[-1;1.0]
-	gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(position_in, 1.0);
+	// - this value represent a position in NDC space (normalized device coordintes), i.e the cube [-1.0;1.0]x[-1;1.0]x[-1;1.0]
+	// PROCEDURAL GENERATION:
+	// - gl_VertexID and gl_InstanceID are predined GLSL variables
+	// - we can use them to specify data for each vertex and each instance
+	// - let first create a parameter space (u,v) in [0.0;1.0]
+	float u = float(gl_VertexID) / float(uNbVertices);
+	float v = float(gl_InstanceID) / float(uNbInstances);
+	// - now remap to [-0.5;0.5] as was the previous square when we defined geometry manually on CPU
+	vec2 position = vec2(u, v) - 0.5;
+	// - write position
+	gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
@@ -49,9 +49,6 @@ void main()
 var fragmentShader =
 `#version 300 es
 precision highp float;
-
-// INPUT
-in vec2 v_textureCoord;
 
 // OUTPUT
 // - the default GL framebuffer is a RGBA color buffer
@@ -64,38 +61,37 @@ out vec4 oFragmentColor;
 // - "uniforms" are variables on GPU in "constant memory" => there values are constant during a "draw command" such as drawArrays()
 // - they can be seen as user custom parameters of your shaders
 // - they can be accessed in any shader (vertex, fragment)
-uniform vec3 uMeshColor;
+uniform float uTime;
 
 // MAIN PROGRAM
 void main()
 {
+	// Random colors based on time [values are between 0.0 and 1.0]
+	float r = 0.5 * sin(uTime) + 0.5;
+	float g = 0.5 * cos(uTime * 2.0)+ 0.5;
+	float b = 0.5 * sin(uTime * 0.5) + 0.5;
+	vec3 meshColor = vec3(r, g , b);
+	
 	// MANDATORY
 	// - a fragment shader MUST write an RGBA color
-	oFragmentColor = vec4(uMeshColor, 1); // [values are between 0.0 and 1.0]
+	oFragmentColor = vec4(meshColor, 1); // [values are between 0.0 and 1.0]
 }
 `;
 
 //--------------------------------------------------------------------------------------------------------
-// GLOBAL VARIABLES
+// Global variables
 //--------------------------------------------------------------------------------------------------------
-
-// Shader program
 var shaderProgram = null;
-var texture = null;
-
-var cube_rend = null;
-
-
+var vao = null;
 // GUI (graphical user interface)
-// - mesh color
-var slider_r;
-var slider_g;
-var slider_b;
+// - procedural generation
+var slider_nbVertices;
+var slider_nbInstances;
 
 //--------------------------------------------------------------------------------------------------------
 // Initialize graphics objects and GL states
 //
-// Here, we want to load a 3D asset
+// Here, we want to display a square/rectangle on screen
 // Uniforms are used to be able edit GPU data with a customized GUI (graphical user interface)
 //--------------------------------------------------------------------------------------------------------
 function init_wgl()
@@ -105,38 +101,37 @@ function init_wgl()
 	ewgl.continuous_update = true;
 	
 	// CUSTOM USER INTERFACE
-	// - this will enable to use GPU "uniform" variables to be able to edit GPU constant variables (at rendering stage)
+	// - used with "uniform" variables to be able to edit GPU constant variables
 	UserInterface.begin(); // name of html id
-		// MESH COLOR
-	    // - container (H: horizontal)
-		UserInterface.use_field_set('H', "Mesh Color");
-			// - sliders (name, min, max, default value, callback called when value is modified)
-			// - update_wgl() is called to refresh screen
-			slider_r  = UserInterface.add_slider('R ', 0, 100, 0, update_wgl);
-			UserInterface.set_widget_color(slider_r,'#ff0000','#ffcccc');
-			slider_g  = UserInterface.add_slider('G ', 0, 100, 100, update_wgl);
-			UserInterface.set_widget_color(slider_g,'#00bb00','#ccffcc');
-			slider_b  = UserInterface.add_slider('B ', 0, 100, 100, update_wgl);
-			UserInterface.set_widget_color(slider_b, '#0000ff', '#ccccff');
+		// PROCEDURAL GENERATION
+		// - container (H: horizontal)
+		UserInterface.use_field_set('H', "Procedural Generation");
+		slider_nbVertices = UserInterface.add_slider('Nb Vertices', 1, 100, 10, update_wgl);
+		slider_nbInstances = UserInterface.add_slider('Nb Instances', 1, 100, 10, update_wgl);
 		UserInterface.end_use();
 	UserInterface.end();
 	
 	// Create and initialize a shader program // [=> Sylvain's API - wrapper of GL code]
 	shaderProgram = ShaderProgram(vertexShader, fragmentShader, 'basic shader');
+	
+	texture = gl.createTexture();
+    const image = new Image;
+    image.src = 'textures/heightmap.png';
+    image.onload = () => {
+        gl.bindTexture(gl.TEXTURE_2D,texture);
+        gl.texImage2D(gl.TEXTURE_2D,0,gl.RED,gl.RED,gl.UNSIGNED_BYTE,image);
+        gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S, gl.REPEAT);
+		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T, gl.REPEAT);
+        gl.bindTexture(gl.TEXTURE_2D,null);
+    };
 
-	let mesh = Mesh.Cube();
-	cube_rend = mesh.renderer(1, 2, 3);
-
-	ewgl.scene_camera.set_scene_radius(mesh.BB.radius);
-	ewgl.scene_camera.set_scene_center(mesh.BB.center);		
-		
 	// Set default GL states
 	// - color to use when refreshing screen
 	gl.clearColor(0, 0, 0 ,1); // black opaque [values are between 0.0 and 1.0]
-	// - enable "depth test"
-	// => to optimize rendering when lots of triangles
-	// => you cannot control triangles ordering at rendering, but "z-buffer" prevent from rendering triangles behind previously rendered triangles
-	gl.enable(gl.DEPTH_TEST);
+	// - no depth buffer
+	gl.disable(gl.DEPTH_TEST);
 }
 
 //--------------------------------------------------------------------------------------------------------
@@ -148,63 +143,42 @@ function draw_wgl()
 	// [1] - always do that
 	// --------------------------------
 	
-	// Clear the GL "color" and "depth" framebuffers (with OR)
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	// Clear the GL color framebuffer
+	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	// --------------------------------
 	// [2] - render your scene
 	// --------------------------------
 	
-	// [part A] : set "current" shader program
-	
-	// Set "current" shader program
+	// // Set "current" shader program
 	shaderProgram.bind(); // [=> Sylvain's API - wrapper of GL code]
 
-	// [part B] : set/modify GPU "uniform" variables of current shader program
-	
 	// Set uniforms // [=> Sylvain's API - wrapper of GL code]
-	//
-	// NOW that the "current" shader program is bound, we can update its assoiated "uniform" variables.
-	// - "uniforms" are variables on GPU in "constant memory" => their values are constant during a "draw command" such as drawArrays()
+	// - "uniforms" are variables on GPU in "constant memory" => there values are constant during a "draw command" such as drawArrays()
 	// - they can be seen as user custom parameters of your shaders
 	// - they can be accessed in any shader (vertex, fragment)
 	// - when creating a "shader program", shader text files are read and analyze by GL functions
 	// - a dictionnary is created with all "uniforms" declared in shader codes
 	// - calling "Uniforms.xxx" is used to update value of a "uniform" on GPU
 	// BEWARE: name MUST be the same as the one declared in your shaders "uniform" variable
+	 // - retrieve current time (float: number of seconds since start of the application) [=> Sylvain's API]
+	Uniforms.uTime = ewgl.current_time;
+	// - procedural generation
 	// - here, we retrieve "slider" values from GUI (graphical user interface)
-	Uniforms.uMeshColor = [slider_r.value/100, slider_g.value/100, slider_b.value/100];
-	// - camera
-	// ---- retrieve current camera matrices ("view" matrix reacts to mouse events)
-	Uniforms.uProjectionMatrix = ewgl.scene_camera.get_projection_matrix();
-	Uniforms.uViewMatrix = ewgl.scene_camera.get_view_matrix();
-	// - set model matrix
-	// ---- configure YOUR custom transformations (scale, rotation, translation)
-    let modelMatrix = Matrix.scale(0.5); // hard-coded "scale" to be able to see the 3D asset
-	Uniforms.uModelMatrix = modelMatrix;
-
-	// [part C] : render your scene (3D model)
+	Uniforms.uNbVertices = slider_nbVertices.value;
+	Uniforms.uNbInstances = slider_nbInstances.value;
 	
-	// Activate texture
-    // TODO : 
-	// - set GL state
-	// - set uniform
-
-	cube_rend.draw(gl.TRIANGLES);
-	
-	// -------------------------------------------------------------------
-	// [3] - Reset the "modified" GL states
-	//     - the graphics card DRIVER hold a list of "current" elements per type (shader program, vao, vbo, ebo, etc...)
-	// -------------------------------------------------------------------
+	// // Draw commands
+	// - same as drawArraysInstanced(), but render a primitive made of "nb vertices", "nb instances" times
+	// - here, we retrieve "slider" values from GUI (graphical user interface)
+	gl.drawArraysInstanced(gl.TRIANGLES, 0/*first index of primitive*/, slider_nbVertices.value, slider_nbInstances.value);
 		
 	// Reset GL state(s)
-	// - unbind vertex array
-	gl.bindVertexArray(null); // not mandatory. For optimization, could be removed.
 	// - unbind shader program
-	gl.useProgram(null); // not mandatory. For optimization, could be removed.
+	gl.useProgram(null);
 }
 
 //--------------------------------------------------------------------------------------------------------
 // => Sylvain's API - call window creation with your customized "init_wgl()" and "draw_wgl()" functions
 //--------------------------------------------------------------------------------------------------------
-ewgl.launch_3d();
+ewgl.launch_2d();
