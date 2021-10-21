dfa98d4c5b5a9501fff4915faed9a7b892cb3124
BEN SAID Walid <wbensaid@pcj404.psi.ad.unistra.fr>
2021-10-21 10:23:38 +0200

    TP5_exercice1

--- easywebgl/TP5.js
+++ easywebgl/TP5.js
@@ -8,39 +8,34 @@ var vertexShader =
 `#version 300 es
 
 // INPUT
-// - the currently bounded vertex array (VAO) contains a VBO of 2D data (positions)
-// - variable is prefixed by "in"
-// - its "location index" MUST be the same value when using vertexAttribPointer() and enableVertexAttribArray() during VAO definition
 layout(location=1) in vec3 position_in;
 layout(location=2) in vec3 normal_in;
 
-//OUTPUT
-out vec3 position;
-out vec3 normal;
-
 // UNIFORM
-// - variable is prefixed by "uniform"
-// - "uniforms" are variables on GPU in "constant memory" => there values are constant during a "draw command" such as drawArrays()
-// - they can be seen as user custom parameters of your shaders
-// - they can be accessed in any shader (vertex, fragment)
-// - Camera
+// - Camera matrices
 uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
-// - Model
+// - Model matrix
 uniform mat4 uModelMatrix;
-uniform mat3 uViewModelMatrix;
+// - Normal matrix: used to transform "vectors"
+uniform mat3 uNormalMatrix;
+
+// OUPUT
+// - these "per-vertex" values will be sent through the graphics pipeline to be interpolated by the hardware rasterizer and retrieved in the fragment shader
+out vec3 v_position;
+out vec3 v_normal;
 
 // MAIN PROGRAM
 void main()
 {
-	// MANDATORY
-	// - a vertex shader MUST write the value of the predined variable " (GLSL langage)"
-	// - this value represent a position in "clip-space"
-	// - This is the space just before dividing coordinates "xyz" by their "w" to lie in NDC space (normalized device coordintes),
-	// - i.e a cube in [-1.0;1.0]x[-1;1.0]x[-1;1.0]
-	gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4( position_in, 1.0 );
-	position = vec3(uViewMatrix * uModelMatrix * vec4(position_in,1.0)) ; 
-	normal = uViewModelMatrix * normal_in;
+	// --------------------------------------
+	// Lighting and shading: PER-FRAGMENT
+	// - here, we "send" mandatory information to the fragment shader (i.e. "position" and "normal")
+	// --------------------------------------
+	v_position = ( uViewMatrix * uModelMatrix * vec4( position_in, 1.0 ) ).xyz; // in View space
+	v_normal = normalize( uNormalMatrix * normal_in ); // in View space
+	
+	gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4( position_in, 1.0 ); // NOTE: this could be optimized with "gl_Position = uProjectionMatrix * p;"
 }
 `;
 
@@ -49,49 +44,68 @@ void main()
 //--------------------------------------------------------------------------------------------------------
 var fragmentShader =
 `#version 300 es
-#define PI (3.141592653589793)
 precision highp float;
 
-// INPUT
-in vec3 position;
-in vec3 normal;
+#define M_PI 3.14159265358979
 
-// OUPUT
-// - the default GL framebuffer is a RGBA color buffer
-// - you can choose whatever name you want
-// - variable is prefixed by "out"
-out vec4 oFragmentColor;
+// INPUT
+// Per-fragment "interpolated" directions of "normal", "light" and "viewer" directions (between each vertices of triangle primitives)
+// - these previous "per-vertex" values have been sent through the graphics pipeline to be interpolated by the hardware rasterizer and retrieved in the fragment shader
+in vec3 v_position;
+in vec3 v_normal;
 
 // UNIFORM
-// - variable is prefixed by "uniform"
-// - "uniforms" are variables on GPU in "constant memory" => there values are constant during a "draw command" such as drawArrays()
-// - they can be seen as user custom parameters of your shaders
-// - they can be accessed in any shader (vertex, fragment)
-uniform vec3 uLightIntensity;
+// Material (BRDF: bidirectional reflectance distribution function)
+uniform vec3 uKa; // ambiant
+uniform vec3 uKd; // diffuse
+uniform vec3 uKs; // specular
+uniform float uNs; // specular
+// Light (Point light)
+uniform float uLightIntensity;
 uniform vec3 uLightPosition;
-uniform vec3 uKa;
-uniform vec3 uKd;
-uniform vec3 uKs;
-uniform float uNs;
+
+// OUPUT
+out vec4 oFragmentColor;
 
 // MAIN PROGRAM
 void main()
 {
-	vec3 lightDirection = normalize(uLightPosition-position);
-	vec3 normalizedNormal = normalize(normal);
-	vec3 viewDirection = normalize(uLightPosition-position);
-	float d = pow(dot(lightDirection,lightDirection),2.0);
-
-	// MANDATORY
-	// - a fragment shader MUST write an RGBA color
-	vec3 iA = uLightIntensity * uKa;
-	vec3 iD = uLightIntensity/d * uKd * max(0., dot(normalizedNormal,lightDirection));
-	vec3 iS = uLightIntensity * uKs * max(0.,pow(dot(normalizedNormal,normalize(lightDirection+viewDirection)),uNs));
-	vec3 normalize_iD = iD/PI;
-	vec3 normalize_iS = iS/((uNs+2.0)/(2.0*PI));
-	vec3 intensity = iA + normalize_iD + normalize_iS;
+	// --------------------------------------
+	// Lighting and shading: PER-FRAGMENT
+	// - here, we "retrieve" mandatory information from the vertex shader (i.e. "position" and "normal")
+	// --------------------------------------
+	vec3 p = v_position;
+	vec3 n = normalize( v_normal ); // interpolated normal direction from current interpolated position in View space
 	
-	oFragmentColor = vec4( intensity, 1 ); // [values are between 0.0 and 1.0]
+	// We use the additive ADS model (ambiant, diffuse, specular) with Blinn Phong equation
+
+	// 1) Reflected ambiant intensity
+	vec3 Ia = uLightIntensity * uKa; // or just vec3 Ia = uKa;
+
+	// 2) Reflected diffuse intensity
+	// vec3 ligthPosition = vec3( 0.0, 0.0, 0.0 ); // here, we use a "hard-coded" light position located on the eye
+	vec3 lightDir = uLightPosition - p; // "light direction" from current interpolated position in View space
+	float d2 = dot(lightDir, lightDir);	// square distance from the light to the fragment
+	lightDir /= sqrt(d2); // normalization of light dir -- or : lightDir = normalize(lightDir);
+	float diffuseTerm = max( 0.0, dot( n, lightDir ) ); // "max" is used to avoir "back" lighting (when light is behind the object)
+	vec3 Id = (uLightIntensity / d2) * uKd * vec3( diffuseTerm );
+	Id = Id / M_PI; // normalization of the diffuse BRDF (for energy conservation)
+	
+	// 3) Reflected specular intensity
+	vec3 Is = vec3( 0.0 );
+	if ( diffuseTerm > 0.0 )
+	{
+		vec3 viewDir = normalize( -p.xyz ); // "view direction" from current vertex position => because, in View space, "dir = vec3( 0.0, 0.0, 0.0 ) - p"
+		vec3 halfDir = normalize( viewDir + lightDir ); // half-vector between view and light vectors
+		float specularTerm = max( 0.0, pow( dot( n, halfDir ), uNs ) ); // "Ns" control the size of the specular highlight (the rugosity)
+		Is = uLightIntensity * uKs * vec3( specularTerm );
+		Is /= (uNs + 2.0) / (2.0 * M_PI); // normalization of the specular BRDF (for energy conservation)
+	}
+	// Reflected intensity (i.e final color)
+	vec3 color = (0.3 * Ia) + (0.3 * Id) + (0.3 * Is);
+	// --------------------------------------
+	
+	oFragmentColor = vec4( color, 1 ); // [values are between 0.0 and 1.0]
 }
 `;
 
@@ -103,21 +117,30 @@ void main()
 var shaderProgram = null;
 
 // GUI (graphical user interface)
-// - mesh color
-var slider_r;
-var slider_g;
-var slider_b;
+// - light color
+// var slider_r;
+// var slider_g;
+// var slider_b;
+// - light position
+var slider_x;
+var slider_y;
+var slider_z;
+// - light intensity
+var slider_light_intensity;
 // - rendering
 var checkbox_wireframe;
 
 // Scene Management
-// - during rendering, OpenGL requires each VAO (vertex array) and its associated number of indices (of each points of triangles) of each sub-mesh of the 3D (here: car model)
+// During rendering, OpenGL requires each VAO (vertex array) and its associated number of indices (of each points of triangles) of each sub-mesh of the 3D (here: car model)
+// - geometry info
 var asset_vao_list = [];
 var asset_nbIndices_list = [];
+// - material info
 var asset_material_ka_list = [];
 var asset_material_kd_list = [];
 var asset_material_ks_list = [];
 var asset_material_ns_list = [];
+
 //--------------------------------------------------------------------------
 // Utility function
 // - no need to understand it for the TP
@@ -224,15 +247,17 @@ function SceneManager_add( object )
 	
 	// [2] - VBO: normal buffer
 	//
+	// Create and initialize a vertex buffer object (VBO) [it is a buffer of generic user data: positions, normals, texture coordinates, temperature, etc...]
+	// - create data on CPU
 	// - we store 3D normales as 1D array : (nx0,ny0,nz0, nx1,ny1,nz1, nx2,ny2,nz2, ...)
 	// IMPORTANT : there are no "normals" stored in the JSON files used in this TP, so we need to compute them with the provided utility function SceneManager_calculateNormals()
-	let normals_array = new Float32Array(SceneManager_calculateNormals(object.vertices,object.indices));
+	let data_normals = new Float32Array( SceneManager_calculateNormals( object.vertices, object.indices ) );
 	// - create a VBO (kind of memory pointer or handle on GPU)
 	let vbo_normals = gl.createBuffer();
-	// - bind "current" VBO (so that each next GL method will be associated to bound element)
-	gl.bindBuffer( gl.ARRAY_BUFFER, vbo_normals);
+	// - bind "current" VBO
+	gl.bindBuffer( gl.ARRAY_BUFFER, vbo_normals ); // HELP: could be seen as a GPU pointer (or handle), ex: "void* vbo_normals"
 	// - allocate memory on GPU (size of data) and send data from CPU to GPU
-	gl.bufferData( gl.ARRAY_BUFFER, normals_array, gl.STATIC_DRAW ); // HELP: could be seen as an allocation on GPU "void* vbo_positions = new float[ data_positions.length ]"
+	gl.bufferData( gl.ARRAY_BUFFER, data_normals, gl.STATIC_DRAW ); // HELP: could be seen as an allocation on GPU "void* vbo_normals = new float[ data_normals.length ]"
 	// - reset GL state
 	gl.bindBuffer( gl.ARRAY_BUFFER, null );
 	
@@ -293,20 +318,19 @@ function SceneManager_add( object )
 	// [B]: normal buffer
 	//----------------------
 	// - bind "current" VBO
-	// HELP: the VAO is going to store the vbo_positions GPU pointer ("void* vbo_positions") at a given attribute ID (see next GL call)
+	// HELP: the VAO is going to store the vbo_normals GPU pointer ("void* vbo_normals") at a given attribute ID (see next GL call)
 	gl.bindBuffer( gl.ARRAY_BUFFER, vbo_normals );
 	// - attach VBO to VAO
 	// - tell how data is stored in "current" VBO in terms of size and format.
 	// - it specifies the "location" and data format of the array of generic vertex attributes at "index" ID to use when rendering
-	let normalsAttributeID = 2;
-	let normalsDataSize = 3; // 3 for 3D positions. Specifies the number of components per generic vertex attribute. Must be 1, 2, 3, 4.;
-	let normalsDataType = gl.FLOAT; // data type
-	gl.vertexAttribPointer( normalsAttributeID, normalsDataSize, normalsDataType,
-							false, 0, 0 );
-	// HELP: vertexAttribPointer() tell VAO to store the VBO handle at given attribute index "void* vbo_positions"
+	vertexAttributeID = 2; // specifies the "index" of the generic vertex attribute to be modified
+	dataSize = 3; // 3 for 3D normals. Specifies the number of components per generic vertex attribute. Must be 1, 2, 3, 4.
+	dataType = gl.FLOAT; // data type
+	gl.vertexAttribPointer( vertexAttributeID, dataSize, dataType,
+							false, 0, 0 ); // unused parameters for the moment (normalized, stride, pointer)
+	// HELP: vertexAttribPointer() tell VAO to store the VBO handle at given attribute index "void* vbo_normals"
 	// - enable the use of VBO. It enable or disable a generic vertex attribute array
-	gl.enableVertexAttribArray( normalsAttributeID ); 
-
+	gl.enableVertexAttribArray( vertexAttributeID );
 	//----------------------
 	// [C]: index buffer
 	//----------------------
@@ -370,7 +394,7 @@ function SceneManager_loadByParts( path, count )
 //--------------------------------------------------------------------------------------------------------
 // Initialize graphics objects and GL states
 //
-// Here, we want to display a square/rectangle on screen
+// Here, we want to load a 3D asset
 // Uniforms are used to be able edit GPU data with a customized GUI (graphical user interface)
 //--------------------------------------------------------------------------------------------------------
 function init_wgl()
@@ -380,23 +404,30 @@ function init_wgl()
 	ewgl.continuous_update = true;
 	
 	// CUSTOM USER INTERFACE
-	// - used with "uniform" variables to be able to edit GPU constant variables
+	// - this will enable to use GPU "uniform" variables to be able to edit GPU constant variables (at rendering stage)
 	UserInterface.begin(); // name of html id
-		// MESH COLOR
+		// LIGHT POSITION
 	    // - container (H: horizontal)
-		UserInterface.use_field_set( 'H', "Mesh Color" );
+		UserInterface.use_field_set( 'H', "LIGHT Position" );
 			// - sliders (name, min, max, default value, callback called when value is modified)
 			// - update_wgl() is callrd to refresh screen
-			slider_r  = UserInterface.add_slider( 'R ', 0, 100, 0, update_wgl );
-			UserInterface.set_widget_color( slider_r,'#ff0000','#ffcccc' );
-			slider_g  = UserInterface.add_slider( 'G ', 0, 100, 100, update_wgl );
-			UserInterface.set_widget_color( slider_g,'#00bb00','#ccffcc');
-			slider_b  = UserInterface.add_slider( 'B ', 0, 100, 100, update_wgl );
-			UserInterface.set_widget_color( slider_b, '#0000ff', '#ccccff' );
+			slider_x  = UserInterface.add_slider( 'X ', -100, 100, 0, update_wgl );
+			UserInterface.set_widget_color( slider_x,'#ff0000','#ffcccc' );
+			slider_y  = UserInterface.add_slider( 'Y ', -100, 100, 80, update_wgl );
+			UserInterface.set_widget_color( slider_y,'#00bb00','#ccffcc');
+			slider_z  = UserInterface.add_slider( 'Z ', -100, 100, 30, update_wgl );
+			UserInterface.set_widget_color( slider_z, '#0000ff', '#ccccff' );
 		UserInterface.end_use();
-		// - Rendering
+		// LIGHT Intensity
+	    // - container (H: horizontal)
+		UserInterface.use_field_set( 'H', "LIGHT Intensity" );
+			// - sliders (name, min, max, default value, callback called when value is modified)
+			// - update_wgl() is callrd to refresh screen
+			slider_light_intensity  = UserInterface.add_slider( 'intensity', 0, 50, 20, update_wgl );
+		UserInterface.end_use();
+		// RENDERING
 		// - container (H: horizontal)
-		UserInterface.use_field_set( 'H', "Rendering" );
+		UserInterface.use_field_set( 'H', "RENDERING Mode" );
 			checkbox_wireframe  = UserInterface.add_check_box( 'wireframe', false, update_wgl );
 		UserInterface.end_use();
 	UserInterface.end();
@@ -408,7 +439,10 @@ function init_wgl()
 	let dataPath = 'models/nissan-gtr/part'; // BEWARE: the "models" directory HAVE TO be placed in the "tp" directory near your javascript file for the TP
 	let nbMeshes = 178; // this car model is a 3D model that has been splitted into 178 pieces
 	SceneManager_loadByParts( dataPath, nbMeshes );
-		
+    
+    
+    var texture = TextureCubeMap();
+    texture.load(["textures/skybox/px.jpg,textures/skybox/nx.jpg,textures/skybox/py.jpg,textures/skybox/ny.jpg,textures/skybox/pz.jpg,textures/skybox/nz.jpg"]);
 	// Set default GL states
 	// - color to use when refreshing screen
 	gl.clearColor( 0, 0, 0 ,1 ); // black opaque [values are between 0.0 and 1.0]
@@ -434,9 +468,13 @@ function draw_wgl()
 	// [2] - render your scene
 	// --------------------------------
 	
+	// [part A] : set "current" shader program
+	
 	// Set "current" shader program
 	shaderProgram.bind(); // [=> Sylvain's API - wrapper of GL code]
 
+	// [part B] : set/modify GPU "uniform" variables of current shader program
+	
 	// Set uniforms // [=> Sylvain's API - wrapper of GL code]
 	//
 	// NOW that the "current" shader program is bound, we can update its assoiated "uniform" variables.
@@ -448,22 +486,27 @@ function draw_wgl()
 	// - calling "Uniforms.xxx" is used to update value of a "uniform" on GPU
 	// BEWARE: name MUST be the same as the one declared in your shaders "uniform" variable
 	// - here, we retrieve "slider" values from GUI (graphical user interface)
-	//Uniforms.uMeshColor = [ slider_r.value/100, slider_g.value/100, slider_b.value/100 ];
 	// - camera
 	// ---- retrieve current camera matrices ("view" matrix reacts to mouse events)
 	Uniforms.uProjectionMatrix = ewgl.scene_camera.get_projection_matrix();
-	let viewMatrix = ewgl.scene_camera.get_view_matrix();
+	const viewMatrix = ewgl.scene_camera.get_view_matrix();
 	Uniforms.uViewMatrix = viewMatrix;
-
 	// - set model matrix
 	// ---- configure YOUR custom transformations (scale, rotation, translation)
     let modelMatrix = Matrix.scale( 0.02 ); // hard-coded "scale" to be able to see the 3D asset
 	Uniforms.uModelMatrix = modelMatrix;
-
-	let view_model = Matrix.mult(viewMatrix,modelMatrix);
-	Uniforms.uViewModelMatrix = view_model.inverse3transpose();
-	Uniforms.uLightPosition = Vec3(0,0,0);
-	Uniforms.uLightIntensity = Vec3(2,2,2);
+	// - model-view matrix
+	let mvm = Matrix.mult(viewMatrix, modelMatrix); // Model-view matrix
+	// - normal matrix
+	Uniforms.uNormalMatrix = mvm.inverse3transpose();
+	// LIGHT
+	// - light color
+	// Uniforms.uLightIntensity = [ slider_r.value/100, slider_g.value/100, slider_b.value/100 ];
+	Uniforms.uLightIntensity = slider_light_intensity.value;
+	Uniforms.uLightPosition = mvm.transform(Vec3(slider_x.value, slider_y.value, slider_z.value)); // to get the position in the View space
+	
+	// [part C] : render your scene (3D model)
+	
 	// Draw commands
 	// - rendering mode
 	let drawMode = gl.TRIANGLES;
@@ -475,22 +518,26 @@ function draw_wgl()
 	let count = asset_vao_list.length;
 	for ( let i = 0; i < count; i++ )
 	{
-		Uniforms.uKa = asset_material_ka_list[i];
-		Uniforms.uKd = asset_material_kd_list[i];
-		Uniforms.uKs = asset_material_ks_list[i];
-		Uniforms.uNs = asset_material_ns_list[i];
+		// Set MATERIAL properties of "current" sub-mesh
+		// - ambient
+		Uniforms.uKa = asset_material_ka_list[ i ];
+		// - diffuse
+		Uniforms.uKd = asset_material_kd_list[ i ];
+		// - specular
+		Uniforms.uKs = asset_material_ks_list[ i ];
+		Uniforms.uNs = asset_material_ns_list[ i ];
+	
 		// Bind "current" vertex array (VAO)
 		gl.bindVertexArray( asset_vao_list[ i ] );
-		
+
 		// Draw command
 		// - render primitives from "current" array data (i.e. bounded VAO)
 		// - during rendering stage, a call to "glDrawArrays()" or "glDrawElements()" will retrieve all its data on GPU from the currently bound VAO
 		// - in the "vertex shader", all declared "IN" variables are fetched from currently bound VBO at given "location ID" of the currently bound VAO
 		// - that's why the VAO MUST store the handle (i.e. kind of pointer) of all its associated VBOs at dedicated index in an array if indices (called "attribute index")
 		gl.drawElements( drawMode, asset_nbIndices_list[ i ]/*number of vertex indices*/, gl.UNSIGNED_INT/*data type in EBO index buffer*/, 0/*not used*/ );
-    }
-	var texture = TextureCubeMap();
-    texture.load(["textures/skybox/cubeMap1.png","textures/skybox/cubeMap2.png","textures/skybox/cubeMap3.png","textures/skybox/cubeMap4.png","textures/skybox/cubeMap5.png","textures/skybox/cubeMap6.png"]);
+	}
+	
 	// -------------------------------------------------------------------
 	// [3] - Reset the "modified" GL states
 	//     - the graphics card DRIVER hold a list of "current" elements per type (shader program, vao, vbo, ebo, etc...)
