771084ccfad81743e39aa76de1cfd31cdee08de4
BEN SAID Walid <wbensaid@pcj404.psi.ad.unistra.fr>
2021-11-17 17:27:33 +0100

    TP7 Première passe

--- easywebgl/TP7_deffered_shading.js
+++ easywebgl/TP7_deffered_shading.js
@@ -1,10 +1,53 @@
 
 "use strict"
 
+var vertexFirstPasse =
+`#version 300 es
+// INPUT
+layout (location=0) in vec3 position_in;
+layout (location=1) in vec3 normal_in;
+
+// UNIFORM
+uniform mat4 uProjectionMatrix;
+uniform mat4 uViewMatrix;
+
+// OUTPUT
+out vec4 v_pos;
+out vec4 v_norm;
+
+// MAIN PROGRAM
+void main()
+{	
+	v_pos = uViewMatrix * vec4(position_in, 1.);
+	v_norm = uViewMatrix * vec4(normal_in, 1.);
+	gl_Position = vec4(position_in, 1.); 
+}
+`;
+
+var fragmentFirstPasse =
+`#version 300 es
+// INPUT
+in vec4 v_pos;
+in vec4 v_norm;
+
+// OUTPUT
+layout (location=0) out vec4 position_out;
+layout (location=1) out vec4 normal_out;
+layout (location=2) out vec4 color_out;
+
+// MAIN PROGRAM
+void main()
+{
+	position_out = v_pos;
+	normal_out = v_norm;
+	color_out = vec4(1., 1., 1., 1.);
+}
+`;
+
 //--------------------------------------------------------------------------------------------------------
 // VERTEX SHADER (GLSL language)
 //--------------------------------------------------------------------------------------------------------
-var vertexShader =
+var vertexSecondPasse =
 `#version 300 es
 
 // INPUT
@@ -35,7 +78,7 @@ void main()
 //--------------------------------------------------------------------------------------------------------
 // FRAGMENT SHADER (GLSL language)
 //--------------------------------------------------------------------------------------------------------
-var fragmentShader =
+var fragmentSecondPasse =
 `#version 300 es
 precision highp float;
 
@@ -49,9 +92,12 @@ in vec3 v_normal;
 // Material (BRDF: bidirectional reflectance distribution function)
 // uniform vec3 uKd; // diffuse
 // Light (Point light)
-uniform vec3 pos_lum[500];
-uniform vec3 col_lum[500];
+uniform vec3 pos_lum[50];
+uniform vec3 col_lum[50];
 uniform int nb_lum;
+uniform sampler2D uSampler1;
+uniform sampler2D uSampler2;
+uniform sampler2D uSampler3;
 
 // OUTPUT
 out vec4 oFragmentColor;
@@ -93,6 +139,7 @@ void main()
 
 // Shader program
 var shaderProgram = null;
+var fullscreen_shaderProgram = null;
 
 // GUI (graphical user interface)
 var lights_intensity;
@@ -100,11 +147,15 @@ var lights_intensity;
 // Renderers
 var cube_rend = null;
 
+var tex = [];
+var fbo = null;
+var fboTexHeight = 250;
+var fboTexWidth = 250;
 //
 var lightsPos = [];
 var lightsColor = [];
 var nbLights = 10;
-var nbLightsMax = 100;
+var nbLightsMax = 50;
 var nbCubes;
 
 //--------------------------------------------------------------------------------------------------------
@@ -126,7 +177,8 @@ function init_wgl()
 	UserInterface.end();
 	
 	// Create and initialize a shader program // [=> Sylvain's API - wrapper of GL code]
-	shaderProgram = ShaderProgram(vertexShader, fragmentShader, 'basic shader');
+	shaderProgram = ShaderProgram(vertexSecondPasse, fragmentSecondPasse, 'basic shader');
+	fullscreen_shaderProgram = ShaderProgram(vertexFirstPasse, fragmentFirstPasse, 'fullscreen shader')
 
 	// Compute a VBO to send the center of cubes (one position vec3 for each cube)
 	var size = 10;	// nb cubes on a line
@@ -172,6 +224,55 @@ function init_wgl()
 		lightsColor.push(Vec3(0.2 + Math.random(), 0.2 + Math.random(), 0.3 + Math.random()));
 	}
 
+	// -------------------------------------------------------------------
+	// Offscreen Rendering: FBO (framebuffer object)
+	// -------------------------------------------------------------------
+	
+	// 1) TEXTURES
+	
+						for(let i = 0;i<3;i++){
+							tex[i] = gl.createTexture();
+						
+							// Bind texture as the "current" one
+							// - each following GL call will affect its internal state
+							gl.bindTexture(gl.TEXTURE_2D, tex[i]);
+								
+							// Configure data type (storage on GPU) and upload image data to GPU
+							gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA32F, fboTexWidth, fboTexHeight, 0, gl.RGBA, gl.FLOAT, null);
+								
+							// Configure "filtering" mode
+							// gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+							// gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+							gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+							gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+								
+							// Clean GL state
+							gl.bindTexture(gl.TEXTURE_2D, null);
+						}
+	// 2) FBO
+	
+						// Generate the FBO (framebuffer object)
+						fbo = gl.createFramebuffer();
+						
+						// - bind "fbo" as the "current" FBO (so that following command will modify its internal state)
+						gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+				
+						gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex[0], 0);
+						gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT1, gl.TEXTURE_2D, tex[1], 0);
+						gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT2, gl.TEXTURE_2D, tex[2], 0);
+						
+						// Specifies a list of color buffers to be drawn into:
+						// - set the target for the fragment shader outputs
+						gl.drawBuffers([gl.COLOR_ATTACHMENT0,gl.COLOR_ATTACHMENT1,gl.COLOR_ATTACHMENT2]); // could be a list, ex: [gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT1]
+
+						let depthRenderBuffer = gl.createRenderbuffer();
+						gl.bindRenderbuffer(gl.RENDERBUFFER, depthRenderBuffer)
+						gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT24,fboTexWidth,fboTexHeight);
+						gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT,gl.RENDERBUFFER,depthRenderBuffer);
+						
+						// - reset GL state (unbind the framebuffer, and revert to default)
+						gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+						gl.bindRenderbuffer(gl.RENDERBUFFER, null);
 	// Set default GL states
 	// - color to use when refreshing screen
 	gl.clearColor(0, 0, 0 , 1); // black opaque [values are between 0.0 and 1.0]
@@ -201,11 +302,36 @@ function update_lights_color()
 //--------------------------------------------------------------------------------------------------------
 function draw_wgl()
 {
+	
+	
+	// -------------------------------------------------------------------
+	// Offscreen Rendering: FBO (framebuffer object)
+	// -------------------------------------------------------------------
+	
+	// - bind "fbo" as the "current" FBO (so that following rendering commands will render data in its buffers [colors, depth])
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+	
+	// - set the viewport for the texture
+	gl.viewport(0/*x*/, 0/*y*/, fboTexWidth/*width*/, fboTexHeight/*height*/);
 	// Clear the GL "color" and "depth" framebuffers (with OR)
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	
 	// Set "current" shader program
+	fullscreen_shaderProgram.bind(); // [=> Sylvain's API - wrapper of GL code]
+	Uniforms.uProjectionMatrix = ewgl.scene_camera.get_projection_matrix();
+	Uniforms.uViewMatrix = ewgl.scene_camera.get_view_matrix();
+	cube_rend.draw(gl.TRIANGLES, nbCubes);
+
+	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+	
+	// - set the viewport for the main window
+	gl.viewport(0/*x*/, 0/*y*/, gl.canvas.width/*width*/, gl.canvas.height/*height*/);
+
+
+	// Set "current" shader program
 	shaderProgram.bind(); // [=> Sylvain's API - wrapper of GL code]
+
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	// - camera
 	// ---- retrieve current camera matrices ("view" matrix reacts to mouse events)
@@ -222,8 +348,18 @@ function draw_wgl()
 	Uniforms.col_lum = cl  ;
 	Uniforms.nb_lum = nbLights;
 	
+	// Samplers
+	gl.activeTexture(gl.TEXTURE0); 
+	gl.bindTexture(gl.TEXTURE_2D, tex[0]);
+	Uniforms.uSampler1 = 0;
+	gl.activeTexture(gl.TEXTURE1); 
+	gl.bindTexture(gl.TEXTURE_2D, tex[1]);
+	Uniforms.uSampler2 = 1;
+	gl.activeTexture(gl.TEXTURE2); 
+	gl.bindTexture(gl.TEXTURE_2D, tex[2]);
+	Uniforms.uSampler3 = 2;
     // render cubes
-	cube_rend.draw(gl.TRIANGLES, nbCubes);
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
 
 	// Reset GL state(s)
 	// - unbind vertex array
